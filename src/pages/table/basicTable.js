7371440a2aba55a5b51f9d324119d5288200aa89
wendy <dttdoutingting@163.com>
2019-12-03 11:30:02 +0800

    add radio and checkbox to basicTable
    Signed-off-by: wendy <dttdoutingting@163.com>

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react';
-import { Card, Table } from 'antd';
+import { Card, Table, Button, Modal, message } from 'antd';
 // import axios from 'axios';
 import axios from './../../axios/index';
 
 class BasicTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedRowKeys: [], // Check here to configure the default column
+      loading: false
+    };
   }
 
   componentWillMount() {
-    const data = [
-      {
-        key: 1,
-        id: 0,
-        userName: 'Jack',
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        key: i,
+        id: i + 1,
+        userName: `Jack${i}`,
         sex: 1,
         state: 3,
         interest: 1,
         birthday: '2000-03-02',
         address: '上海市浦东新区',
         time: '08:30'
-      },
-      {
-        key: 2,
-        id: 1,
-        userName: 'Jack',
-        sex: 1,
-        state: 1,
-        interest: 1,
-        birthday: '2000-03-02',
-        address: '上海市浦东新区',
-        time: '08:30'
-      },
-      {
-        key: 3,
-        id: 2,
-        userName: 'Jack',
-        sex: 2,
-        state: 2,
-        interest: 4,
-        birthday: '2000-03-02',
-        address: '上海市浦东新区',
-        time: '08:30'
-      }
-    ];
+      });
+    }
     this.setState({
       data
     });
@@ -68,16 +50,75 @@ class BasicTable extends Component {
         data: {
           params: {
             page: 1
-          }
+          },
+          isShowLoading: false
         }
       })
       .then(res => {
         if (res.code === 0) {
+          res.result.list.map((item, index) => {
+            item.key = index;
+            return item;
+          });
           this.setState({
-            data2: res.result.list
+            data2: res.result.list,
+            selectedRowKeys: [],
+            selectedRows: null
           });
         }
       });
+  };
+
+  onRowClick = (record, index) => {
+    let selectKey = [index];
+    this.setState({
+      selectedRowKeys1: selectKey,
+      selectedItem: record
+    });
+    Modal.info({
+      title: '信息',
+      content: `用户名：${record.userName}, 用户id：${record.id}`
+    });
+  };
+  onSelectChange = (selectedRowKeys, selectedRows) => {
+    let ids = [];
+    selectedRows.map(item => {
+      ids.push(item);
+      return item;
+    });
+    // selectedRowKeys是必须的
+    this.setState({
+      selectedRowKeys,
+      selectedRows: ids
+    });
+  };
+  start = () => {
+    this.setState({ loading: true });
+    // ajax request after empty completing
+    setTimeout(() => {
+      this.setState({
+        selectedRowKeys: [],
+        loading: false
+      });
+    }, 1000);
+  };
+
+  handleDelete = () => {
+    let rows = this.state.selectedRows;
+    console.log(rows);
+    let ids = [];
+    rows.map(item => {
+      ids.push(item.id);
+      return item;
+    });
+    Modal.confirm({
+      title: '删除提示',
+      content: `您确定要删除这些数据吗？${ids.join(',')}`,
+      onOk: () => {
+        message.success('删除成功');
+        this.request();
+      }
+    });
   };
   render() {
     const columns = [
@@ -133,6 +174,16 @@ class BasicTable extends Component {
         dataIndex: 'time'
       }
     ];
+    const { loading, selectedRowKeys1, selectedRowKeys } = this.state;
+    const rowSelection1 = {
+      type: 'radio',
+      selectedRowKeys1
+    };
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange
+    };
+    const hasSelected = selectedRowKeys.length > 0;
     return (
       <div>
         <Card title="基础表格">
@@ -143,8 +194,45 @@ class BasicTable extends Component {
             // pagination={false}
           ></Table>
         </Card>
-        <Card title="动态数据渲染表格" style={{ marginTop: 10 }}>
+        <Card title="动态数据渲染表格-Mock" style={{ marginTop: 10 }}>
           <Table columns={columns} dataSource={this.state.data2}></Table>
+        </Card>
+        <Card title="表格-单选" style={{ marginTop: 10 }}>
+          <Table
+            rowSelection={rowSelection1}
+            onRow={(record, index) => {
+              return {
+                onClick: () => {
+                  this.onRowClick(record, index);
+                }
+              };
+            }}
+            columns={columns}
+            dataSource={this.state.data}
+          ></Table>
+        </Card>
+        <Card title="表格-复选" style={{ marginTop: 10 }}>
+          <div style={{ marginBottom: 16 }}>
+            <Button
+              type="primary"
+              onClick={this.start}
+              disabled={!hasSelected}
+              loading={loading}
+            >
+              Reload
+            </Button>
+            <span style={{ marginLeft: 8 }}>
+              {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+            </span>
+            <Button style={{ marginLeft: 10 }} onClick={this.handleDelete}>
+              删除
+            </Button>
+          </div>
+          <Table
+            rowSelection={rowSelection}
+            columns={columns}
+            dataSource={this.state.data2}
+          ></Table>
         </Card>
       </div>
     );
