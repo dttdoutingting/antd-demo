d0787d38946d23ade8f9bf6b0f480b7179cbfc30
wendy <dttdoutingting@163.com>
2019-12-06 12:57:29 +0800

    add home page to router
    Signed-off-by: wendy <dttdoutingting@163.com>

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import Login from './pages/login';
 import Admin from './admin';
+import Home from './pages/home';
 import Buttons from './pages/ui/buttons';
 import Modals from './pages/ui/modals';
 import Loadings from './pages/ui/loadings';
@@ -35,6 +36,7 @@ class IRouter extends Component {
             render={() => (
               <Admin>
                 <Switch>
+                  <Route path="/admin/home" component={Home}></Route>
                   <Route path="/admin/ui/buttons" component={Buttons}></Route>
                   <Route path="/admin/ui/modals" component={Modals}></Route>
                   <Route path="/admin/ui/loadings" component={Loadings}></Route>
